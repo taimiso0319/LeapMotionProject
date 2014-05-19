6592a6c83b7c8a8c75dcfadf5f2a31e62c14ab62
Keisuke Yasuda <keisuke0319@gmail.com>
2014-05-20 06:07:36 +0900

    NewRepository

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class Counter : MonoBehaviour {
+	
+	Controller controller;
+	
+	public int Hs;
+	public int Fs;
+	public float o;
+	public float h;
+	public float n;
+	public float k;
+	public float c;
+
+
+	// Use this for initialization
+	void Start () {
+		controller = new Controller();
+
+	}
+	// Update is called once per frame
+	void Update () {
+		if(controller.IsConnected) //controller is a Controller object
+		{
+			Frame frame = controller.Frame(); // controller is a Controller object
+			HandList hands = frame.Hands;
+			Hs = hands.Count;
+			FingerList fingers = frame.Fingers;
+			Fs = fingers.Count;
+			
+			o = frame.Fingers.FingerType(0).Count;
+		}
+	}
+}
