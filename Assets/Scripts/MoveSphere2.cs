4dcd26e5f8159d694a09a83647271e1f532364f0
Keisuke Yasuda <keisuke0319@gmail.com>
2014-06-18 01:14:45 +0900

    merge

---
+++ Assets/Scripts/MoveSphere2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSphere2 : MonoBehaviour {
+
+	FingerPosition fingerPosition;
+	public float x,y,z;
+	public int Hand_Type;
+	public int Finger_Type;
+	public int Joint_Type;
+	// Use this for initialization
+	void Start () {
+		fingerPosition = (FingerPosition)GameObject.Find("_Manager").GetComponent<FingerPosition>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
